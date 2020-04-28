0be77fe98857ff9c503d1c28bdfe47ff012c930a
stefan <stefanheijden1@gmail.com>
2020-04-28 18:17:26 +0200

    Added Tour and Spotify page

--- src/components/Info-Sheets/SP-Sheet.vue
+++ src/components/Info-Sheets/SP-Sheet.vue
@@ -4,21 +4,59 @@
     height="100%"
   >
   <!--Titel-->
-    <v-row
-      class="fill-height"
-      align="top"
-      justify="center"
-    >
-      <div class="display-4">Spotify Page</div>
+    <v-row>
+      <v-col cols="12" >
+        <v-card>
+          <v-container fluid>
+            <v-row>
+              <v-col
+                v-for="{name, image, url} in albums"
+                :key="name"
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-card flat tile class="d-flex">
+                  <v-img
+                    :src="image"
+                     v-on:click="select(url)"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                    <template>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                      <div class="display-3" >{{name}}</div>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
     </v-row>
   </v-sheet>
 </template>
 
 <script>
+import image from "./../../assets/Observers.png"
+
 export default {
   name: 'spsheet',
+  methods: {
+    select: function(url) {
+      window.location.href = url;
+    }
+  },
   data () {
     return {
+      albums: [
+        {name: "Observers", image: image, url: "https://open.spotify.com/album/6OfqOB3kZ1jlC3wGpcrQ9m"},
+      ],
     }
   },
 }
--- src/components/Info-Sheets/Tour-Sheet.vue
+++ src/components/Info-Sheets/Tour-Sheet.vue
@@ -4,12 +4,28 @@
     height="100%"
   >
   <!--Titel-->
-    <v-row
-      class="fill-height"
-      align="top"
-      justify="center"
-    >
-      <div class="display-4">Tours</div>
+    <v-row class="mx-2">
+      <v-col
+        cols="12"
+      >
+      <!--Titel-->
+        <v-row
+          align="center"
+          class="mr-0"
+        >
+          <v-spacer/>
+            <div class="display-4">Tours</div>
+          <v-spacer/>
+        </v-row>
+        <v-row
+          align="center"
+          class="mr-0"
+        >
+          <v-spacer/>
+            <div class="display-3">No tours so far :(</div>
+          <v-spacer/>
+        </v-row>
+      </v-col>
     </v-row>
   </v-sheet>
 </template>
